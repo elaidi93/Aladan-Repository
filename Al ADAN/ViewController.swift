de53e55a6b065d32beead4dfacbb3a9897b25ae6
Hamza El Aidi <hamza@hassans-imac.local>
2015-12-23 14:41:10 +0000

    Initial Commit

---
+++ Al ADAN/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Al ADAN
+//
+//  Created by Hamza El Aidi on 12/23/15.
+//  Copyright © 2015 Hamza El Aidi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
